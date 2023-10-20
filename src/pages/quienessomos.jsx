9437f2a4b2413393fe97b752c572dd888037d300
Your Name <you@example.com>
2023-10-19 23:46:21 -0300

    arreglos 10

--- src/pages/quienessomos.jsx
+++ src/pages/quienessomos.jsx
@@ -12,7 +12,7 @@ const Quienessomos = () => {
 
       <div className="main-content">
         <CarrucelPaginas
-          imagen="https://res.cloudinary.com/dcwloh062/image/upload/v1697736653/Sin_t%C3%ADtulo-1_2_zp79i7.png"
+          imagen="https://res.cloudinary.com/dcwloh062/image/upload/v1697769543/Sin_t%C3%ADtulo-1_3_ycts0g.png"
         />
       </div>
       
