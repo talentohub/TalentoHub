d4b038e27e9034df8747516d3714ecedf426d5cd
Your Name <you@example.com>
2023-10-19 23:08:02 -0300

    arreglos 9

--- src/componentes/formulario.jsx
+++ src/componentes/formulario.jsx
@@ -1,14 +1,27 @@
-
 import { useState } from 'react';
-import emailjs from "emailjs-com";
-emailjs.init('iEjJ4Hc_tJX5mcHWN')
-import "../css/formulario.css"
+import emailjs from 'emailjs-com';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faUser,faPhone,faEnvelope,faComment } from '@fortawesome/free-solid-svg-icons';
+import { faUser, faPhone, faEnvelope, faComment } from '@fortawesome/free-solid-svg-icons';
 
+emailjs.init('iEjJ4Hc_tJX5mcHWN');
 
 const Formulario = () => {
   const [submitting, setSubmitting] = useState(false);
+  const [formData, setFormData] = useState({
+    name: '',
+    last: '',
+    phone: '',
+    email: '',
+    message: ''
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value
+    });
+  };
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -22,6 +35,13 @@ const Formulario = () => {
         () => {
           setSubmitting(false);
           alert('Mensaje Enviado Correctamente');
+          setFormData({
+            name: '',
+            last: '',
+            phone: '',
+            email: '',
+            message: ''
+          });
         },
         (err) => {
           setSubmitting(false);
@@ -29,81 +49,89 @@ const Formulario = () => {
         }
       );
   };
-  
-    return (
-        <>
-      <form id="form" onSubmit={handleSubmit}>
-      <div className="input-group mb-3">
-          <span className="input-group-text">
-            <i className="fa fa-user"><FontAwesomeIcon icon={faUser} /></i> 
-          </span>
-          
-          <input
-            type="text"
-            className="form-control"
-            placeholder="Tu Nombre"
-            name="name"
-            required
-          />
-        </div>
-        <div className="input-group mb-3">
-          <span className="input-group-text">
-            <i className="fa fa-user"><FontAwesomeIcon icon={faUser} /></i> 
-          </span>
-          <input
-            type="text"
-            className="form-control"
-            placeholder="Tu Apellido"
-            name="last"
-            required
-          />
-        </div>
-        <div className="input-group mb-3">
-          <span className="input-group-text">
-            <FontAwesomeIcon icon={faPhone} /> 
-          </span>
-          <input
-            type="number"
-            className="form-control"
-            placeholder="Tu Numero de Contacto"
-            name="phone"
-            required
-          />
-        </div>
-        <div className="input-group mb-3">
-          <span className="input-group-text">
-            <FontAwesomeIcon icon={faEnvelope} /> 
-          </span>
-          <input
-            type="email"
-            className="form-control"
-            placeholder="Tu Correo Electronico"
-            aria-describedby="emailHelp"
-            name="email"
-            required
-          />
-          
-        </div>
-        <div className="input-group mb-3">
-          <span className="input-group-text">
-            <FontAwesomeIcon icon={faComment} />
-          </span>
-          <textarea
-            className="form-control"
-            placeholder="Tu Mensaje"
-            rows="3"
-            name="message"
-            required
-          ></textarea>
-        </div>
 
-        <button type="submit" className="btn btn-success" disabled={submitting}>
+  return (
+    <form id="form" onSubmit={handleSubmit}>
+      <div className="input-group mb-3">
+        <span className="input-group-text">
+          <i className="fa fa-user">
+            <FontAwesomeIcon icon={faUser} />
+          </i>
+        </span>
+        <input
+          type="text"
+          className="form-control"
+          placeholder="Tu Nombre"
+          name="name"
+          required
+          value={formData.name}
+          onChange={handleChange}
+        />
+      </div>
+      <div className="input-group mb-3">
+        <span className="input-group-text">
+          <i className="fa fa-user">
+            <FontAwesomeIcon icon={faUser} />
+          </i>
+        </span>
+        <input
+          type="text"
+          className="form-control"
+          placeholder="Tu Apellido"
+          name="last"
+          required
+          value={formData.last}
+          onChange={handleChange}
+        />
+      </div>
+      <div className="input-group mb-3">
+        <span className="input-group-text">
+          <FontAwesomeIcon icon={faPhone} />
+        </span>
+        <input
+          type="number"
+          className="form-control"
+          placeholder="Tu Numero de Contacto"
+          name="phone"
+          required
+          value={formData.phone}
+          onChange={handleChange}
+        />
+      </div>
+      <div className="input-group mb-3">
+        <span className="input-group-text">
+          <FontAwesomeIcon icon={faEnvelope} />
+        </span>
+        <input
+          type="email"
+          className="form-control"
+          placeholder="Tu Correo Electronico"
+          aria-describedby="emailHelp"
+          name="email"
+          required
+          value={formData.email}
+          onChange={handleChange}
+        />
+      </div>
+      <div className="input-group mb-3">
+        <span className="input-group-text">
+          <FontAwesomeIcon icon={faComment} />
+        </span>
+        <textarea
+          className="form-control"
+          placeholder="Tu Mensaje"
+          rows="3"
+          name="message"
+          required
+          value={formData.message}
+          onChange={handleChange}
+        ></textarea>
+      </div>
+
+      <button type="submit" className="btn btn-success" disabled={submitting}>
         {submitting ? 'Enviando...' : 'Enviar'}
       </button>
-       
-      </form>
-      
-    </>
+    </form>
   );
 };
 
