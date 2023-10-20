f54802ac3edee13a2ab7cc2617002ca1ed7b5e55
Your Name <you@example.com>
2023-10-19 22:59:11 -0300

    arreglos 8

--- src/componentes/navbar.jsx
+++ src/componentes/navbar.jsx
@@ -6,7 +6,7 @@ const Navbar = () => {
     <>
       <nav className="navbar navbar-expand-lg">
         <div className="container-fluid">
-          <a className="navbar-brand" href="#">
+          <a className="navbar-brand" href="inicio">
             <div className="logo">
               <img className="logo-img" src={TH} alt="Logo" />
             </div>
